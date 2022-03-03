0b500df8fd1ad0d102017386104128c00dd78319
Vivian H Kwan <cs15lwi22acy@ieng6-202.ucsd.edu>
2022-03-02 18:27:31 -0800

    added directory functionality

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,11 +1,35 @@
 
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MarkdownParse {
+    public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
+        Map<String, List<String>> result = new HashMap<>();
+        if(dirOrFile.isDirectory()) {
+            for(File f: dirOrFile.listFiles()) {
+                result.putAll(getLinks(f));
+            }
+            return result;
+        }
+        else {
+            Path p = dirOrFile.toPath();
+            int lastDot = p.toString().lastIndexOf(".");
+            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
+                return result;
+            }
+            ArrayList<String> links = getLinks(Files.readString(p));
+            result.put(dirOrFile.getPath(), links);
+            return result;
+        }
+    }
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
@@ -44,7 +68,7 @@ public class MarkdownParse {
                     }
 
                 } else {
-                    System.out.println(markdown.indexOf("\n", openParen));
+                    // System.out.println(markdown.indexOf("\n", openParen));
                     currentIndex = markdown.indexOf("\n", openParen) - 1;
                     pastCloseParen = 0;
                 }
@@ -55,9 +79,19 @@ public class MarkdownParse {
     }
 
     public static void main(String[] args) throws IOException {
+        // Path fileName = Path.of(args[0]);
+        // String contents = Files.readString(fileName);
+        // ArrayList<String> links = getLinks(contents);
+        // System.out.println(links);
         Path fileName = Path.of(args[0]);
-        String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+        File dirOrFile = new File(args[0]);
+        if (!dirOrFile.isDirectory()) {
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = getLinks(contents);
+            System.out.println(links);
+        } else {
+            Map<String, List<String>>links = getLinks(dirOrFile);
+            System.out.println(links);
+        }
     }
 }
